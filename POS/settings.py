070b13c7aa29526f205d2991ac1e0e2c4dec5510
chonnasit <tong25658@gmail.com>
2021-03-04 22:40:52 +0700

    fix  setting

--- POS/settings.py
+++ POS/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'POS'
 ]
 
 MIDDLEWARE = [
@@ -79,8 +80,13 @@ WSGI_APPLICATION = 'POS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME':'pos',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
 
--- projectpos/models.py
+++ projectpos/models.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+class Owner(models.Model):
+    email = models.charFileld(max_length=255)
+    password = models.charFileld(max_length=255)
