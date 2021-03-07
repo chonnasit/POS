aa57e0dee84f04fc145895583b9260f30c9e6217
chonnasit <tong25658@gmail.com>
2021-03-07 17:58:16 +0700

    fix

--- projectpos/models.py
+++ projectpos/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # Create your models here.
 class Owners(models.Model):
-    #id  = models.AutoField(auto_created=True,primary_key=True)
+    id  = models.AutoField(auto_created=True,primary_key=True)
     firstname = models.CharField(max_length=255)
     Lastname = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
@@ -17,9 +17,9 @@ class Owners(models.Model):
     store_name= models.CharField(max_length=200)
 
 class Table(models.Model):
-    #id  = models.AutoField(auto_created=True,primary_key=True)
+    id  = models.AutoField(auto_created=True,primary_key=True)
     number = models.CharField(max_length=3)
-    #type_order=models.CharField(max_length=200)
+    type_order=models.CharField(max_length=200)
     people = models.CharField(max_length=2)
     time = models.CharField(max_length=10)
     order = models.CharField(max_length=255)
