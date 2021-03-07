2fcca5dbc7390200024537f26d63f3a1a505fa12
chonnasit <tong25658@gmail.com>
2021-03-07 17:48:42 +0700

    fix

--- POS/settings.py
+++ POS/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME':'djangodb',
+        'NAME':'pos',
         'USER':'root',
         'PASSWORD':'',
         'HOST':'127.0.0.1',
